6e0530bca541cb0d063cb92a4404109e7fafabeb
Youssef Salah72 <yzmn2003@gmail.com>
2025-01-06 21:24:03 +0200

    make "oncreate function , user table , product table , sales table"

--- lib/core/database/sqflite/my_sqflite_database.dart
+++ lib/core/database/sqflite/my_sqflite_database.dart
@@ -2,17 +2,43 @@ import 'package:sqflite/sqflite.dart' as sqfliteDatabase;
 import 'package:sqflite__departement/core/database/sqflite/crud.dart';
 import 'package:path/path.dart';
 class MySqfliteDatabase extends CRUD {
+  static const String _userTable = "users";
+  static const String _userColumnId = "user_id";
+  static const String _userColumnUsername = "username";
+  static const String _userColumnPassword = "password";
+  static const String _productTable = "product";
+  static const String _productColumnId = "product_id";
+  static const String _productColumnUsername = "username";
+  static const String _productColumnPrice = "price";
+  static const String _productColumnProductCount = "product_count";
+  static const String _salesTable = "sales";
+  static const String _salesColumnId = "sales_id";
+  static const String _salesColumnProductName = "product_name";
+  static const String _salesColumnUsername = "sales_username";
+
   initDatabase() async {
     String databasesPath = await sqfliteDatabase.getDatabasesPath();
     String managementDatabaseName = "management.db";
     String realDatabasePath = join(databasesPath, managementDatabaseName);
     int versionDatabase = 1;
     sqfliteDatabase.openDatabase(realDatabasePath,
-    onCreate: (db, version) {
-
-    },
+    onCreate: _oncreate,
       version: versionDatabase
     );
+  }
+  _oncreate(sqfliteDatabase.Database db, int version) async {
+    await db.execute("CREATE TABLE $_userTable ($_userColumnId INTEGER ,"
+        " $_userColumnUsername TEXT ,"
+        " $_userColumnPassword TEXT)");
+    ////////////////////////
+    await db.execute("CREATE TABLE $_productTable ($_productColumnId INTEGER ,"
+        " $_productColumnUsername TEXT , "
+        " $_productColumnPrice REAL ,"
+        " $_productColumnProductCount INTEGER)");
+    ///////////////////////
+    await db.execute("CREATE TABLE $_salesTable ($_salesColumnId INTEGER ,"
+        " $_salesColumnProductName TEXT ,"
+        " $_salesColumnUsername TEXT)");
   }
 
   @override
